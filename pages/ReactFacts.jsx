6603ee62eacfb1f31e5b93bc14cb944b9014550e
Akuna Joshua <akunajoshua@gmail.com>
2025-12-04 16:19:27 +0100

    Header, MainContent and Footer components creater in ReactFact page

--- pages/ReactFacts.jsx
+++ pages/ReactFacts.jsx
@@ -1,43 +1,63 @@
-export default function ReactFacts(){
-    return (
-        <Page />
-    )
+export default function ReactFacts() {
+  return <Page />;
 }
 
-function TemporaryName(){
-    return (
-        <main className="react react-facts">
-            <img src="react-logo.svg" alt="react logo" width="40px"/>
-            <h1>Fun facts about React</h1>
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walker</li>
-                <li>Has well over 200k stars on Github</li>
-                <li>Is maintained by Meta</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
-        </main>
-    )
+function Header() {
+  return (
+    <header>
+      <img src='react-logo.svg' alt='react logo' width='40px' />
+    </header>
+  );
 }
 
-function Page(){
-    return (
-        <div className="facts">
-            <header>
-                <img src="react-logo.svg" alt="" width="40px" />
-            </header>
-            <main>
-                <h1>Reason I am excited to learn React</h1>
-                <ol className>
-                    <li>React is a popular library, so I will be more likely to
-                        fit in with all the coolest devs out there!
-                    </li>
-                    <li>I am more likely to get a job as a front end developer if I know react.</li>
-                </ol>
-            </main>
-            <footer>
-                <small>&copy; 2024 ziroll development. All rights reserved</small>
-            </footer>
-        </div>
-    )
+function MainContent() {
+  return (
+    <main>
+      <h1>Reason I am excited to learn React</h1>
+      <ol className>
+        <li>
+          React is a popular library, so I will be more likely to fit in with
+          all the coolest devs out there!
+        </li>
+        <li>
+          I am more likely to get a job as a front end developer if I know
+          react.
+        </li>
+      </ol>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>&copy; 2024 ziroll development. All rights reserved</small>
+    </footer>
+  );
+}
+
+function TemporaryName() {
+  return (
+    <main className='react react-facts'>
+      <img src='react-logo.svg' alt='react logo' width='40px' />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walker</li>
+        <li>Has well over 200k stars on Github</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+}
+
+function Page() {
+  return (
+    <div className='facts'>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
 }
