d386d1cc0d8f73e35843656375ac8a440ac526e0
Akuna Joshua <akunajoshua@gmail.com>
2025-12-04 15:52:11 +0100

    ReactFacts page created

--- pages/ReactFacts.jsx
+++ pages/ReactFacts.jsx
@@ -1,5 +1,45 @@
-export default function Page1(){
+import MainContent from "../components/MainContent"
+
+export default function ReactFacts(){
     return (
-        <h1>This is a static page of react facts</h1>
+        <Page />
+    )
+}
+
+function TemporaryName(){
+    return (
+        <main className="react react-facts">
+            <img src="react-logo.svg" alt="react logo" width="40px"/>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walker</li>
+                <li>Has well over 200k stars on Github</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
+
+function Page(){
+    return (
+        <div className="facts">
+            <header>
+                <img src="react-logo.svg" alt="" width="40px" />
+            </header>
+            <main>
+                <h1>Reason I am excited to learn React</h1>
+                <ol className>
+                    <li>React is a popular library, so I will be more likely to
+                        fit in with all the coolest devs out there!
+                    </li>
+                    <li>I am more likely to get a job as a front end developer if I know react.</li>
+                </ol>
+            </main>
+            <footer>
+                <small>&copy; 2024 ziroll development. All rights reserved</small>
+            </footer>
+        </div>
     )
 }
