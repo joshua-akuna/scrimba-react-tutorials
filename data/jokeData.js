3c9084e6a3b109ac737ef82b625360bc62e1f462
Akuna Joshua <akunajoshua@gmail.com>
2025-12-06 15:35:02 +0100

    jokeData array created

---
+++ data/jokeData.js
@@ -0,0 +1,21 @@
+export default [
+    {
+        setup: "I got my daughtera fridge for her birthday.",
+        punchline: "I can't wait to see her face lightup when she opens it."
+    },
+    {
+        setup: "How did the hacker escape the police.",
+        punchline: "He just ransomware!"
+    },
+    {
+        setup: "Why don't pirates travel on mountain road?",
+        punchline: "Scurvy."
+    },
+    {
+        setup: "Why do bees stay in the hive in winter?",
+        punchLine: "Swarm."
+    },{
+        setup: "What's the best thing about Switzerland?",
+        punchline: "I don't know, but the flag is a big plus!"
+    }
+]
