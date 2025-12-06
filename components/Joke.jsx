66b18fa5e543c481eea20b259db595590af70179
Akuna Joshua <akunajoshua@gmail.com>
2025-12-06 11:51:16 +0100

    Jokes page and Joke component updated

--- components/Joke.jsx
+++ components/Joke.jsx
@@ -1,3 +1,9 @@
-export default function Joke() {
-  return <h1>Inside the Joke Component</h1>;
+export default function Joke(props) {
+  return (
+    <>
+      {props.setup && <p className='setup'>Setup: {props.setup}</p>}
+      <p className='punchline'>PunchLine: {props.punchline}</p>
+      <hr />
+    </>
+  );
 }
--- pages/Jokes.jsx
+++ pages/Jokes.jsx
@@ -1,5 +1,25 @@
 import Joke from '../components/Joke';
 
 export default function Jokes() {
-  return <Joke />;
+  return (
+    <main>
+      <Joke punchline="It's hard to explain puns to kleptomaniac because they take things literally" />
+      <Joke
+        setup='How did the hacker escape the police.'
+        punchline='He just ransomware!'
+      />
+      <Joke
+        setup="Why don't pirates travel on mountain road?"
+        punchline='Scurvy.'
+      />
+      <Joke
+        setup='Why do bees stay in the hive in winter?'
+        punchline='Swarm.'
+      />
+      <Joke
+        setup="What's the best thing about Switzerland?"
+        punchline="I don't know, but the flag is a big plus!"
+      />
+    </main>
+  );
 }
