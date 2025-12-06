018d749dc8b7587dbc08738d3c16be9556d0f23c
Akuna Joshua <akunajoshua@gmail.com>
2025-12-06 17:24:27 +0100

    journal-entry.js created and journal.js file updated

--- data/jornal.js
+++ data/jornal.js
@@ -2,7 +2,25 @@ export default [
     {
         id: 1,
         img: {
-            src: "https://scrimba.com"
-        }
-    }
+            src: "https://scrimba.com/links/travel-journal-japan-image-url",
+            alt: 'Mount Fuji'
+        },
+        title: "Mount Fuji",
+        country: "Japan",
+        googleMapsLink: "http://www.google.com",
+        dates: '12 Jan, 2021 - 24 Jan, 2021',
+        text: 'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both japanese and foreign tourists'
+    },
+    {
+        id: 2,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-australia-image-url",
+            alt: 'Sydney Opera House'
+        },
+        title: "Sydney Opera House",
+        country: "Australia",
+        googleMapsLink: "http://www.google.com",
+        dates: '27 May, 2021 - 8 June, 2021',
+        text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings"
+    },
 ]
---
+++ data/journal-entry.md
@@ -0,0 +1,18 @@
+img: {
+src: "https://scrimba.com/links/travel-journal-japan-image-url"
+alt: "Mount Fuji"
+}
+title:
+Mount Fuji
+
+country: Japan
+
+googleMapsLink: http://www.google.com/map/place/
+
+dates:
+12 Jan, 2021 - 24 Jan, 2021
+
+text:
+Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
+(12,380 feet). Mount Fuji is the single most popular tourist site in
+Japan, for both japanese and foreign tourists
