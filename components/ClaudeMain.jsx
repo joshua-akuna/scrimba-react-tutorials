9d347c0e930b01d2c51234f1c4bf91660ad020b8
Akuna Joshua <akunajoshua@gmail.com>
2025-12-08 10:44:22 +0100

    ClaudeMain component created

---
+++ components/ClaudeMain.jsx
@@ -0,0 +1,3 @@
+export default function ClaudeMain() {
+  return <h1>Inside Cluade Main</h1>;
+}
--- pages/ChefClaude.jsx
+++ pages/ChefClaude.jsx
@@ -1,5 +1,11 @@
 import Header from '../components/ClaudeHeader';
+import Main from '../components/ClaudeMain';
 
 export default function ChefClaude() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
