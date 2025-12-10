05a02e00eca03c25e1d74241f8a10cd9a660df84
Akuna Joshua <akunajoshua@gmail.com>
2025-12-10 20:28:07 +0100

    arial-label added to button

--- pages/Ternary.jsx
+++ pages/Ternary.jsx
@@ -4,9 +4,10 @@ export default function State() {
   //   const isGoingOut = false;
 
   const [isGoingOut, setIsGoingOut] = useState(false);
+
   let answer = isGoingOut ? 'Yes' : 'No';
 
-  function handleClick() {
+  function changeMind() {
     setIsGoingOut(function (prevState) {
       return !prevState;
     });
@@ -15,7 +16,12 @@ export default function State() {
   return (
     <main className='state-main'>
       <h1>Do I feel like going out tonight?</h1>
-      <button onClick={handleClick}>{answer}</button>
+      <button
+        onClick={changeMind}
+        aria-label={`The current answer is ${answer}. click to change it.`}
+      >
+        {answer}
+      </button>
     </main>
   );
 }
