4d56605b5db2fe5b88d8b8dbf37034aaf58741a4
Akuna Joshua <akunajoshua@gmail.com>
2025-12-10 18:16:00 +0100

    State page updated

--- pages/State.jsx
+++ pages/State.jsx
@@ -1,3 +1,16 @@
+import { useState } from 'react';
+
 export default function State() {
-  return <h1>Inside State</h1>;
+  const [result, setResult] = useState('Yes');
+
+  function handleClick() {
+    setResult('Heck Yes!');
+  }
+
+  return (
+    <main className='state-main'>
+      <h1>Is state important to know?</h1>
+      <button onClick={handleClick}>{result}</button>
+    </main>
+  );
 }
