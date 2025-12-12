b359fd0990a04a22e0fab1be663e906b81092789
Akuna Joshua <akunajoshua@gmail.com>
2025-12-12 19:48:25 +0100

    Messages page updated with a ternary operator

--- pages/Messages.jsx
+++ pages/Messages.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react';
 
 export default function Messages() {
-  const [unreadMessages, setUnreadMessages] = useState(['a', 'b']);
+  const [unreadMessages, setUnreadMessages] = useState([]);
+
+  let prompt =
+    unreadMessages.length == 0
+      ? 'You are all caught up!'
+      : unreadMessages.length == 1
+      ? `You have ${unreadMessages.length} unread message`
+      : `You have ${unreadMessages.length} unread messages`;
 
   return (
     <div>
-      {unreadMessages.length > 0 && (
+      {/* {unreadMessages.length > 0 && (
         <h1>You have {unreadMessages.length} unread messages!</h1>
       )}
-      {unreadMessages.length === 0 && <p>You have no unread messages</p>}
+      {unreadMessages.length === 0 && <p>You have no unread messages</p>} */}
+      <h1>{prompt}</h1>
     </div>
   );
 }
