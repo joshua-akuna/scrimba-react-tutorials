908fd26f45481bfd663eea1662ef719a53c8b3cd
Akuna Joshua <akunajoshua@gmail.com>
2025-12-13 13:56:06 +0100

    Pad component created and Pads page refactored for Pad component

--- pages/Pads.jsx
+++ pages/Pads.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import padData from '../data/pads';
+import Pad from '../components/Pad';
 
 export default function Pads() {
   const [pads, setPads] = useState(padData);
 
-  const padElements = pads.map((pad) => <button key={pad.id}></button>);
+  const padElements = pads.map((pad) => <Pad key={pad.id} color={pad.color} />);
 
   return <main className='pads'>{padElements}</main>;
 }
