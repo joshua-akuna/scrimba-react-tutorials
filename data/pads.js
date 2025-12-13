164fd361355d4e78dcc377fe41df4f3a1262e16b
Akuna Joshua <akunajoshua@gmail.com>
2025-12-13 12:57:51 +0100

    pads.js data created

---
+++ data/pads.js
@@ -0,0 +1,42 @@
+export default [
+    {
+        id: 1,
+        color: #F18D88,
+        on: true
+    },
+    {
+        id: 2,
+        color: #F5C280,
+        on: false
+    },
+    {
+        id: 3,
+        color: #EEE679,
+        on: true
+    },
+    {
+        id: 4,
+        color: #64ED98,
+        on: true
+    },
+    {
+        id: 5,
+        color: #63DEED,
+        on: false
+    },
+    {
+        id: 6,
+        color: #877FED,
+        on: false
+    },
+    {
+        id: 7,
+        color: #52a567ff,
+        on: false
+    },
+    {
+        id: 8,
+        color: #a69c52ff,
+        on: false
+    },
+]
