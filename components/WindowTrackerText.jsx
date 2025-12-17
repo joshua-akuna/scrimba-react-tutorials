66a0281cd6e72752725dcb25d0cd5fbbd0643a95
Akuna Joshua <akunajoshua@gmail.com>
2025-12-17 18:33:08 +0100

    implemented clean up code for the window add event listener

--- components/WindowTrackerText.jsx
+++ components/WindowTrackerText.jsx
@@ -1,3 +1,26 @@
+import { useEffect, useState } from 'react';
+
 export default function WindowTrackerText() {
-  return <h1>Window width: {window.innerWidth}</h1>;
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    // this is the callback function that runs
+    // when an event is triggered
+    function watchWindowWidth() {
+      // use setWinowWidth anytime the resize event triggers
+      console.log('resized');
+      setWindowWidth(window.innerWidth);
+    }
+
+    // add a resize event listener to the window
+    window.addEventListener('resize', watchWindowWidth);
+
+    // window.removeEventListener()
+    return function () {
+      console.log('cleaning up');
+      window.removeEventListener('resize', watchWindowWidth);
+    };
+  }, []);
+
+  return <h1>Window width: {windowWidth}</h1>;
 }
